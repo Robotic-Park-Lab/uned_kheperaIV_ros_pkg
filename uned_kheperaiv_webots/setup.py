cb4cc7e94327129e689a9dfa36595ef32debd57d
FranciscoJManasAlvarez <fjmanas@dia.uned.es>
2022-09-23 13:54:11 +0200

    Webots plugin creation

---
+++ uned_kheperaiv_webots/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'uned_kheperaiv_webots'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kiko',
+    maintainer_email='fjmanas@dia.uned.es',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'khepera_driver = uned_kheperaiv_webots.khepera_driver:main'
+        ],
+    },
+)
---
+++ uned_kheperaiv_webots/uned_kheperaiv_webots/khepera_driver.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from uned_kheperaiv_webots.')
+
+
+if __name__ == '__main__':
+    main()
