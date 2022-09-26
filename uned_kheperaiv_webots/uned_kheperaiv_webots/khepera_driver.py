09283e1181d9fbde24e66607da9fa691434f5d88
FranciscoJManasAlvarez <fjmanas@dia.uned.es>
2022-09-26 15:06:57 +0200

    Position: P Controller

--- uned_kheperaiv_webots/uned_kheperaiv_webots/khepera_driver.py
+++ uned_kheperaiv_webots/uned_kheperaiv_webots/khepera_driver.py
@@ -1,14 +1,16 @@
+from cmath import sqrt
 from rosgraph_msgs.msg import Clock
 import rclpy
 import os
+import math
 from rclpy.node import Node
 from rclpy.time import Time
 
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Pose
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
 
-from math import cos, sin, degrees, radians, pi
+from math import atan2, cos, sin, degrees, radians, pi
 import sys
 import tf_transformations
 from tf2_ros import TransformBroadcaster
@@ -32,9 +34,22 @@ class KheperaWebotsDriver:
 
         self.target_twist = Twist()
 
+        ## Init Pose
+        self.global_x = 0.0
+        self.global_y = 0.0
+        self.global_yaw = 0.0
+
+        self.target_pose = Pose()
+        self.target_pose.position.x = 0.0
+        self.target_pose.position.y= 0.0
+
         ## Initialize Sensors
         self.gyro = self.robot.getDevice("gyro")
         self.gyro.enable(timestep)
+        self.gps = self.robot.getDevice("gps")
+        self.gps.enable(timestep)
+        self.imu = self.robot.getDevice("inertial unit")
+        self.imu.enable(timestep)
 
         self.range_left = self.robot.getDevice("left ultrasonic sensor")
         self.range_left.enable(timestep)
@@ -56,12 +71,14 @@ class KheperaWebotsDriver:
         self.node = rclpy.create_node(name_value+'_driver')
         self.node.get_logger().info('Webots_Node::inicialize() ok. %s' % (str(name_value)))
         self.node.create_subscription(Twist, name_value+'/cmd_vel', self.cmd_vel_callback, 1)
+        self.node.create_subscription(Pose, name_value+'/goal_pose', self.goal_pose_callback, 1)
         self.laser_publisher = self.node.create_publisher(LaserScan, name_value+'/scan', 10)
+        self.msg_laser = LaserScan()
+        self.node.create_timer(1.0/30.0, self.publish_laserscan_data)
 
         self.tfbr = TransformBroadcaster(self.node)
     
-        self.msg_laser = LaserScan()
-        self.node.create_timer(1.0/30.0, self.publish_laserscan_data)
+        
 
     def publish_laserscan_data(self):
         left_range = self.range_left.getValue()
@@ -95,12 +112,38 @@ class KheperaWebotsDriver:
     def cmd_vel_callback(self, twist):
         self.target_twist = twist
 
+    def goal_pose_callback(self, pose):
+        self.target_pose = pose
+
     def step(self):
         rclpy.spin_once(self.node, timeout_sec=0)
 
         dt = self.robot.getTime() - self.past_time
 
-        self.motor_left.setVelocity((2*self.target_twist.linear.x/0.021-0.10540*self.target_twist.angular.z/0.021))
-        self.motor_right.setVelocity((2*self.target_twist.linear.x/0.021+0.10540*self.target_twist.angular.z/0.021))
+        # Get pose
+        self.global_x = self.gps.getValues()[0]
+        self.global_y = self.gps.getValues()[1]
+        self.global_yaw = self.imu.getRollPitchYaw()[2]
+        
+        # TO-DO: Position Controller
+        distance_error = sqrt(pow(self.target_pose.position.x-self.global_x,2)+pow(self.target_pose.position.y-self.global_y,2))
+        angle_error = -self.global_yaw+atan2(self.target_pose.position.y-self.global_y,self.target_pose.position.x-self.global_x)
+        
+        if distance_error.real>0.01:
+            self.target_twist.linear.x = distance_error.real*cos(angle_error)
+        else:
+            self.target_twist.linear.x = 0.0
+        self.target_twist.angular.z = sin(angle_error)*10
+
+        # Velocity [rad/s]
+        self.motor_left.setVelocity((self.target_twist.linear.x/0.042-0.10540*self.target_twist.angular.z))
+        self.motor_right.setVelocity((self.target_twist.linear.x/0.042+0.10540*self.target_twist.angular.z))
+
+        # TO-DO: Introudcir Modelo Cinemático Diferencial Directo -> /odom
 
         self.past_time = self.robot.getTime()
+
+        # DEBUG
+        self.node.get_logger().info('Get Pose3D: X:%f Y:%f yaw:%f' % (self.gps.getValues()[0],self.gps.getValues()[1],self.imu.getRollPitchYaw()[2]))
+        self.node.get_logger().info('Target Pose3D: X:%f Y:%f' % (self.target_pose.position.x,self.target_pose.position.y))
+        self.node.get_logger().info('Target Pose3D: Distance:%f Angle:%f' % (distance_error.real,angle_error))
